1141ef9cd27d3674771043d081cef230f42a3a0c
zsy2053 <zsy2053@gmail.com>
2023-05-17 14:45:10 -0400

    Improved vector db

--- src/main/routes/bots.py
+++ src/main/routes/bots.py
@@ -1,14 +1,21 @@
 import uuid
 import os
+import json
+import numpy as np
 import asyncio, concurrent.futures
 import semantic_kernel as sk
 from semantic_kernel.connectors.ai.open_ai import OpenAITextCompletion, OpenAITextEmbedding, AzureTextCompletion
 from flask import Blueprint, request, jsonify, make_response
 from typing import List
+from datetime import datetime, timezone
+from sentence_transformers import SentenceTransformer, util
 import semantic_kernel as sk
+from bson.objectid import ObjectId
+from sklearn.metrics.pairwise import cosine_similarity
 from src.main.routes import user_token_required, bot_api_key_required, get_client
 
 bots_routes = Blueprint('bots_routes', __name__)
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 
 kernel = sk.Kernel()
@@ -21,17 +28,23 @@ kernel.add_text_embedding_generation_service("ada", OpenAITextEmbedding("text-em
 kernel.register_memory_store(memory_store=sk.memory.VolatileMemoryStore())
 kernel.import_skill(sk.core_skills.TextMemorySkill())
 
-context = None
+sk_prompt = """
+ChatBot can have a conversation with you about any topic.
+It can give explicit instructions or say 'I don't know' if
+it does not have an answer.
+
+Chat:
+{{$chat_history}}
+User: {{$user_input}}
+ChatBot: """.strip()
+
+context = kernel.create_new_context()
+context["chat_history"] = ""
+
+chat_func = kernel.create_semantic_function(sk_prompt, max_tokens=200, temperature=0.8)
 pool = concurrent.futures.ThreadPoolExecutor()
-chat_func = None
 
-async def populate_memory(collection_param: str, bot_answers: List[str]) -> None:
-    # Add some documents to the semantic memory
-    global kernel
-    for index, item in enumerate(bot_answers):
-        await kernel.memory.save_information_async(collection_param, item['text'], item['id'])
-
-def setup_chat_with_memory(collection_param: str, relevance_score: float) -> None:
+def setup_chat_with_memory(relevance_score: float) -> None:
     global context
     global chat_func
     global kernel
@@ -50,10 +63,7 @@ def setup_chat_with_memory(collection_param: str, relevance_score: float) -> Non
 
     chat_func = kernel.create_semantic_function(sk_prompt, max_tokens=200, temperature=relevance_score)
 
-    context[sk.core_skills.TextMemorySkill.COLLECTION_PARAM] = collection_param
-    context[sk.core_skills.TextMemorySkill.RELEVANCE_PARAM] = relevance_score
-
-async def talk_bot(user_input: str, collection_name: str, relevance_score: float) -> dict:
+async def talk_bot(user_input: str, file_name: str, relevance_score: float) -> dict:
     global context
     global chat_func
     global kernel
@@ -66,10 +76,15 @@ async def talk_bot(user_input: str, collection_name: str, relevance_score: float
         print("\n\nExiting chat...")
         return ""
 
-    result = await kernel.memory.search_async(collection_name, user_input)
-    if result[0].relevance >= relevance_score:
-        context["chat_history"] += f"\nUser:> {user_input}\nChatBot:> {result[0].text}\n"
-        return result[0].text
+    data = {}
+    with open(file_name) as j:
+        data = json.load(j)
+    vectors_input = model.encode(user_input)
+    res = cosine_similarity([vectors_input], data['vectors'])
+    max_index = np.argmax(res)
+    max_value = np.max(res)
+    if max_value >= relevance_score:
+        return data['texts'][max_index]
 
     default_answer = await kernel.run_async(chat_func, input_vars=context.variables)
     context["chat_history"] += f"\nUser:> {user_input}\nChatBot:> {default_answer}\n"
@@ -80,27 +95,35 @@ async def talk_bot(user_input: str, collection_name: str, relevance_score: float
 @user_token_required
 def add_collection(current_user):
     payload = request.json
+    if payload['collection_list'] is None:
+        return make_response(jsonify({'error': 'Collection items must not be none'}), 400)
+    res = {}
+    code_list = []
+    vectors = model.encode(payload['collection_list'])
+    arr = np.array(vectors)
+    arr_as_list = arr.tolist()
+    res['vectors'] = arr_as_list
+    res['texts'] = payload['collection_list']
+    current_date_time = datetime.now(timezone.utc)
+    file_name = 'Documents/' + current_user['id'] + '/' + payload['collection_name'] + '.json'
+    os.makedirs(os.path.dirname(file_name), exist_ok=True)
+    with open(file_name, 'w+') as f:
+        json.dump(res, f)
     db = get_client()
-    bots = db['bots']
-    bots.insert_one({'id': str(uuid.uuid4()),
-        'user_id': current_user['id'],
-        'collection_name': payload['collection_name'],
-        'collection_list': payload['collection_list'],
-        'relevance_score': payload['relevance_score']})
-    return make_response(jsonify({'data': 'New db collection added'}), 201)
+    users = db['users']
+    if 'my_files' in current_user:
+        current_user['my_files'].append(file_name)
+    else:
+        current_user['my_files'] = [file_name]
+    users.update_one({'id': current_user['id']}, {'$set':{'my_files': current_user['my_files']}})
+    return make_response(jsonify({'data': 'New collection added'}), 201)
 
-@bots_routes.route('/populate_kernel', methods=['POST'])
+@bots_routes.route('/reset_context', methods=['POST'])
 @user_token_required
-def populate_kernel(current_user):
+def reset_context(current_user):
     payload = request.json
-    db = get_client()
-    bots = db['bots']
-    find_bot = bots.find_one({'user_id': current_user['id'], 'id': payload['collection_id']})
-    if not find_bot:
-        return make_response(jsonify({'error': 'Bot not found'}), 400)
-    setup_chat_with_memory(find_bot['collection_name'], find_bot['relevance_score'])
-    pool.submit(asyncio.run, populate_memory(find_bot['collection_name'], find_bot['collection_list'])).result()
-    return make_response(jsonify({'collection_name': find_bot['collection_name'], 'relevance_score': find_bot['relevance_score'], 'message': 'Kernel populated successfully'}), 200)
+    setup_chat_with_memory(payload['relevance_score'])
+    return make_response(jsonify({'message': 'Context refreshed'}), 200)
 
 @bots_routes.route('/chat', methods=['POST'])
 @user_token_required
@@ -108,5 +131,8 @@ def chat(current_user):
     payload = request.json
     if payload is None or payload['input'] is None:
         return make_response(jsonify({'error': 'Must provide input key'}), 400)
-    result = pool.submit(asyncio.run, talk_bot(payload['input'], payload['collection_name'], payload['relevance_score'])).result()
+    file_name = 'Documents/' + current_user['id'] + '/' + payload['collection_name']
+    if file_name not in current_user['my_files']:
+        return make_response(jsonify({'error': 'User does\'t have this file'}), 400)
+    result = pool.submit(asyncio.run, talk_bot(payload['input'], file_name, payload['relevance_score'])).result()
     return make_response(jsonify({'data': result}), 200)
