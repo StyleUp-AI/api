4decbaf2cbd6df9730c0925829e0e04664d22306
zsy2053 <zsy2053@gmail.com>
2023-06-12 00:24:06 -0400

    Updated url

--- src/main/routes/bots.py
+++ src/main/routes/bots.py
@@ -20,7 +20,7 @@ SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 from google_auth_oauthlib.flow import Flow
 
 flow = Flow.from_client_secrets_file(
-    os.path.join(os.getcwd(), "src/main/routes/credentials.json") , SCOPES, redirect_uri="http://localhost:8080/#/google_sign_in"
+    os.path.join(os.getcwd(), "src/main/routes/credentials.json") , SCOPES, redirect_uri="http://localhost:8080/google_sign_in"
 )
 
 bots_routes = Blueprint("bots_routes", __name__)
